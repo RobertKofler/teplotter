52d5ec1bc633ed7b579fea7cfad861ef7b4ec55d
Robert Kofler <rokofler@gmail.com>
2026-07-23 15:40:24 +0200

    minor modi

--- estimate-so.py
+++ estimate-so.py
@@ -16,6 +16,8 @@ parser.add_argument('--so', type=str, default=None,dest="seqentry", required=Tru
 parser.add_argument("--end-distance", type=int, required=False, dest="enddist", default=100, help="distance from ends for normalizing")
 parser.add_argument("--exclude-quantile", type=int, required=False, dest="quantile", default=25, help="exclude the most extreme coverage quantiles for normalizing")
 parser.add_argument("--output-file", type=str, required=False, dest="outfile", default=None, help="output file in so format; if none is provided output will be screen")
+parser.add_argument("--sample-id", type=str, required=False, dest="sampleid", default="x", help="the ID of current sample")
+
 
 args = parser.parse_args()
 writer = Writer(args.outfile)
@@ -23,13 +25,12 @@ writer = Writer(args.outfile)
 # than normalize each entry
 for se in SeqEntryReader(args.seqentry):
     cost=NormFactor.getCovStat(se,args.enddist,args.quantile)
-    topr=[se.seqname,str(len(se.cov))]
-    form=[]
+    topr=[args.sampleid,se.seqname,str(len(se.cov))]
     for c in cost:
         if c is not None:
-            form.append(f"{c:.2f}",)
+            topr.append(f"{c:.2f}",)
         else:
-            form.append("na")
-    topr.extend(form)     
+            topr.append("na")
+    
     tp="\t".join(topr)
     writer.write(tp)
